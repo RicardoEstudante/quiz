a6b94ee0e50fc8193bf1f415e28719c87d7e626a
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2021-01-27 21:39:06 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,44 @@
 import styled from 'styled-components';
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
+// import Widget from '../src/components/Widget'
+// import QuizLogo from '../src/components/QuizLogo'
 import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+// import Footer from '../src/components/Footer'
+// import GitHubCorner from '../src/components/GitHubCorner'
 
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+// export const QuizContainer = styled.div`
+//   width: 100%;
+//   max-width: 350px;
+//   padding-top: 45px;
+//   margin: auto 10%;
+//   @media screen and (max-width: 500px) {
+//     margin: auto;
+//     padding: 15px;
+//   }
+// `;
+
+export const Bloco = styled.div`
+  display: flex;
+  flex: 1;
+
+  align-items: center;
+  justify-content: center;
+`
+
+export const Title = styled.div`
+  font-size: 30px;
+  font-weight: bold;
+  color: black;
+`
 
 export default function Home() {
   return (
-    <QuizBackground backgroundImage={db.bg}>
-      <QuizContainer>
+    <Bloco>
+      <Title>Te amo bobolhuda ❤️</Title>
+    </Bloco>
+);
+     {/* <QuizBackground backgroundImage={db.bg}>
+     <QuizContainer>
         <QuizLogo />
         <Widget>
           <Widget.Header>
@@ -40,7 +58,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto" />
-    </QuizBackground>
-  );
+      <GitHubCorner projectUrl="https://github.com/omariosouto" /> 
+    </QuizBackground> */}
+  
 }
