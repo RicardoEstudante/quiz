1a1a97353042ff59b78c6428cdfcf63b6dc940ea
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2021-01-29 08:00:25 -0300

    finish class one

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
 import styled from 'styled-components';
 import db from '../db.json';
-// import Widget from '../src/components/Widget'
-// import QuizLogo from '../src/components/QuizLogo'
+import Widget from '../src/components/Widget'
+import QuizLogo from '../src/components/QuizLogo'
 import QuizBackground from '../src/components/QuizBackground'
-// import Footer from '../src/components/Footer'
-// import GitHubCorner from '../src/components/GitHubCorner'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
 
-// export const QuizContainer = styled.div`
-//   width: 100%;
-//   max-width: 350px;
-//   padding-top: 45px;
-//   margin: auto 10%;
-//   @media screen and (max-width: 500px) {
-//     margin: auto;
-//     padding: 15px;
-//   }
-// `;
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export const Bloco = styled.div`
   display: flex;
@@ -33,13 +33,11 @@ export const Title = styled.div`
 
 export default function Home() {
   return (
-    <Bloco>
-      <Title>Te amo bobolhuda ❤️</Title>
-    </Bloco>
-);
-     {/* <QuizBackground backgroundImage={db.bg}>
-     <QuizContainer>
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+
         <QuizLogo />
+
         <Widget>
           <Widget.Header>
             <h1>{db.title}</h1>
@@ -56,9 +54,12 @@ export default function Home() {
             <p>lorem ipsum dolor sit amet...</p>
           </Widget.Content>
         </Widget>
+
         <Footer />
+
       </QuizContainer>
+
       <GitHubCorner projectUrl="https://github.com/omariosouto" /> 
-    </QuizBackground> */}
-  
+    </QuizBackground>
+  )
 }
