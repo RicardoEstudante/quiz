7f806aebd707daa7eac3cac106939664a62c8eaf
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2021-02-01 22:49:47 -0300

    1H

--- pages/index.js
+++ pages/index.js
@@ -11,17 +11,7 @@ import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import QuizContainer from '../src/components/QuizContainer';
 
 export const Bloco = styled.div`
   display: flex;
@@ -76,8 +66,7 @@ export default function Home() {
               />
 
               <Button type="submit" disabled={name.length === 0}>
-                Jogar
-                {name}
+                {`Jogar ${name}`}
               </Button>
             </form>
           </Widget.Content>
--- pages/quiz.js
+++ pages/quiz.js
@@ -1,5 +1,101 @@
-export default function Quiz() {
+import React from 'react';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizContainer from '../src/components/QuizContainer';
+import QuizBackground from '../src/components/QuizBackground';
+import Button from '../src/components/Button';
+
+const LoadingWidget = () => (
+  <Widget>
+    <Widget.Header>
+      Carregando...
+    </Widget.Header>
+
+    <Widget.Content>
+      [Desafio do Loading]
+    </Widget.Content>
+  </Widget>
+);
+
+const QuestionWidget = ({ question, totalQuestions, questionIndex }) => {
+  const questionId = `question__${questionIndex}`;
+
   return (
-    <h1>testeadwqdwefefe f wef fwe</h1>
+    <Widget>
+      <Widget.Header>
+        <h3>
+          {`Pergunta ${questionIndex + 1} de${totalQuestions}`}
+        </h3>
+      </Widget.Header>
+
+      <img
+        alt="Descrição"
+        style={{
+          width: '100%',
+          height: '150px',
+          objectFit: 'cover',
+        }}
+        src={question.image}
+      />
+      <Widget.Content>
+        <h2>
+          {question.title}
+        </h2>
+
+        <p>
+          {question.description}
+        </p>
+
+        <form>
+          {question.alternatives.map((alternative, alternativeIndex) => {
+            const alternativeId = `alternative__${alternativeIndex}`;
+
+            return (
+              <Widget.Topic
+                as="label"
+                htmlFor={alternativeId}
+              >
+                <input
+                  style={{ display: 'none' }}
+                  id={alternativeId}
+                  name={questionId}
+                  type="radio"
+                />
+                {alternative}
+              </Widget.Topic>
+            );
+          })}
+
+          {/* <pre>
+            {JSON.stringify(question.alternatives, null, 4)}
+          </pre> */}
+
+          <Button type="submit">
+            Confirmar
+          </Button>
+        </form>
+      </Widget.Content>
+    </Widget>
+  );
+};
+
+export default function Quiz() {
+  const totalQuestions = db.questions.length;
+  const questionIndex = 0;
+  const question = db.questions[questionIndex];
+
+  return (
+    <QuizBackground>
+      <QuizContainer>
+        <QuizLogo />
+        <QuestionWidget
+          question={question}
+          questionIndex={questionIndex}
+          totalQuestions={totalQuestions}
+        />
+        <LoadingWidget />
+      </QuizContainer>
+    </QuizBackground>
   );
 }
